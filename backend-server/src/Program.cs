350b5d50bba5cd51f1450374f32cba8658b4b8c7
MatHyp <matijoker29@gmail.com>
2025-08-01 14:43:35 +0200

    added init .net WebSocketServer logic

---
+++ backend-server/src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using FleckWebSocketApp.WebSockets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var server = new WebSocketServerManager();
+        server.Start();
+
+        Console.WriteLine("Press Enter to exit...");
+        Console.ReadLine();
+    }
+}
---
+++ backend-server/tests/BackendServer.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BackendServer.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
