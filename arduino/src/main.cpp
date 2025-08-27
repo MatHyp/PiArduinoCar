23afe33d4f703e1e41379fcd2e58b6f0f3d4252d
MatHyp <matijoker29@gmail.com>
2025-08-27 21:23:13 +0200

    Feat: latency test on blink LEDs

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -2,6 +2,12 @@
 
 void setup()
 {
+
+  pinMode(8, OUTPUT);
+  digitalWrite(8, LOW);
+
+  pinMode(9, OUTPUT);
+  digitalWrite(9, LOW);
   Serial.begin(9600);
   Serial.println("READY"); // handshake message
 }
@@ -10,8 +16,23 @@ void loop()
 {
   if (Serial.available() > 0)
   {
-    char c = Serial.read();
-    Serial.print("Odebrano: ");
-    Serial.println(c);
+    char c = Serial.read(); // Read one byte
+    Serial.println(c);      // Echo back to Python
+
+    // Blink LED only if the byte is 'D'
+    switch (c)
+    {
+    case 'D':
+      digitalWrite(8, HIGH);
+      delay(100);
+      digitalWrite(8, LOW);
+      break;
+
+    case 'A':
+      digitalWrite(9, HIGH);
+      delay(100);
+      digitalWrite(9, LOW);
+      break;
+    }
   }
 }
